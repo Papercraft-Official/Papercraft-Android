4f881eb8f72f6561e6112d778bff6e80ec2d1c80
lingyicute <160479206+lingyicute@users.noreply.github.com>
2024-10-04 13:24:15 +0800

    Feat server 4

--- TMessagesProj/src/main/java/org/telegram/messenger/browser/Browser.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/browser/Browser.java
@@ -511,7 +511,7 @@ public class Browser {
                 return true;
             }
         } else if (all) {
-            if (host.endsWith("telegram.org") || host.endsWith("telegra.ph") || host.endsWith("telesco.pe")) {
+            if (host.endsWith("telegram.org")) {
                 return true;
             }
         }
